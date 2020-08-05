162df2c0513c9297e055dd77f0b0885fa68f35de
Carl Patrick L. Reyes <carlpatrick.reyes@netgies.com>
2020-08-05 06:17:21 +0000

    Initial commit. Shiny template.

---
+++ app.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+# UI component
+ui <- fluidPage(
+  # Application title
+  titlePanel("Simple Shiny App"),
+  
+  # Sidebar Layout
+  sidebarLayout(
+    sidebarPanel("This is the sidebar panel"),
+    mainPanel("This is the main panel")
+  )
+)
+
+# Server component
+server <- function(input, output) {
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
