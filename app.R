897c6be970f4f11aafa4204b944d5fd5fe60a176
Carl Patrick L. Reyes <carlpatrick.reyes@netgies.com>
2020-08-07 04:19:12 +0000

    Created 'Create Account' section with textInput and passwordInput functions.

--- app.R
+++ app.R
@@ -4,11 +4,19 @@ library(shiny)
 # UI component
 ui <- fluidPage(
   # Application title
-  titlePanel("Simple Shiny App"),
+  titlePanel("Subscribe to Shiny RApp Newsletter"),
   
   # Sidebar Layout
   sidebarLayout(
-    sidebarPanel("This is the sidebar panel"),
+    sidebarPanel(
+      h4("CREATE ACCOUNT"),
+        textInput(inputId = "email", label = "EMAIL ADDRESS", width = "50%"),
+        passwordInput(inputId = "password", label = "PASSWORD", width = "50%"),
+
+      h4("PERSONAL INFORMATION"),
+        textInput(inputId = "lastname", label = "NAME", placeholder = "Last Name",  width = "50%"),
+        textInput(inputId = "firstname", label = "", placeholder = "First Name", width = "50%")
+    ),
     mainPanel("This is the main panel")
   )
 )
