7964b8cc00b5f23fa69533a5cae2780ff0449182
Carl Patrick L. Reyes <carlpatrick.reyes@netgies.com>
2020-08-13 03:18:21 +0000

    update ui. radioButtons. selectInput

--- app.R
+++ app.R
@@ -3,14 +3,14 @@ library(shiny)
 
 # UI component
 ui <- fluidPage(
-  
+
   includeCSS("styles.css"),
-  
+
   # Application title
   titlePanel(
     h1("Subscribe to Shiny RApp Newsletter")
        ),
-  
+
   # Sidebar Layout
   sidebarLayout(
     sidebarPanel(
@@ -19,8 +19,18 @@ ui <- fluidPage(
         passwordInput(inputId = "password", label = "PASSWORD", width = "50%"),
 
       h4("PERSONAL INFORMATION"),
+        # Name
         textInput(inputId = "lastname", label = "NAME", placeholder = "Last Name",  width = "50%"),
-        textInput(inputId = "firstname", label = "", placeholder = "First Name", width = "50%")
+        textInput(inputId = "firstname", label = "", placeholder = "First Name", width = "50%"),
+
+        # Birthday
+        dateInput(inputId = "birthday", label = "BIRTHDAY", value = NULL, format = "yyyy-mm-dd", width = "50%"),
+
+        # Sex
+        radioButtons(inputId = "sex", label = "SEX", choices = c("Male", "Female"), selected = NULL),
+
+        # Marital Status
+        selectInput(inputId = "marital", label = "MARITAL STATUS", choices = c("Single", "Married", "Widowed"), width = "50%")
     ),
     mainPanel(
       h4("SUMMARY"),
