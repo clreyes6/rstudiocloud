950ede5a04622ef95d3d0a360fb15d9bab9d79ba
Carl Patrick L. Reyes <carlpatrick.reyes@netgies.com>
2020-08-17 15:30:50 +0000

    edit ui

--- app.R
+++ app.R
@@ -8,29 +8,41 @@ ui <- fluidPage(
 
   # Application title
   titlePanel(windowTitle = "RApp Newsletter",
-    h1("Subscribe to Shiny RApp Newsletter")
-       ),
+             h1("Subscribe to Shiny RApp Newsletter")
+  ),
 
   # Sidebar Layout
   sidebarLayout(
     sidebarPanel(
-      h4("CREATE ACCOUNT"),
-        textInput(inputId = "email", label = "EMAIL ADDRESS", width = "50%"),
-        passwordInput(inputId = "password", label = "PASSWORD", width = "50%"),
+      h4("CREATE ACCOUNT"), width = 3,
+      textInput(inputId = "email", label = "EMAIL ADDRESS", width = "80%"),
+      passwordInput(inputId = "password", label = "PASSWORD", width = "80%"),
 
       h4("PERSONAL INFORMATION"),
-        # Name
-        textInput(inputId = "lastname", label = "NAME", placeholder = "Last Name",  width = "50%"),
-        textInput(inputId = "firstname", label = "", placeholder = "First Name", width = "50%"),
+      # Name
+      textInput(inputId = "lastname", label = "NAME", placeholder = "Last Name",  width = "80%"),
+      textInput(inputId = "firstname", label = "", placeholder = "First Name", width = "80%"),
 
-        # Birthday
-        dateInput(inputId = "birthday", label = "BIRTHDAY", value = NULL, format = "yyyy-mm-dd", width = "50%"),
+      # Birthday
+      dateInput(inputId = "birthday", label = "BIRTHDAY", value = NULL, format = "yyyy-mm-dd", width = "80%"),
 
-        # Sex
-        radioButtons(inputId = "sex", label = "SEX", choices = c("Male", "Female"), selected = NULL),
+      # Sex
+      radioButtons(inputId = "sex", label = "SEX", choices = c("Male", "Female"), selected = NULL),
 
-        # Marital Status
-        selectizeInput(inputId = "marital", label = "MARITAL STATUS", choices = c("Single", "Married", "Widowed"), selected = NULL, width = "50%", multiple = TRUE, options = list(maxItems = 2))
+      # Marital Status
+      selectizeInput(inputId = "marital", label = "MARITAL STATUS", choices = c("Single", "Married", "Widowed"), selected = NULL, width = "80%", multiple = TRUE, options = list(maxItems = 2)),
+
+      # Language
+      checkboxGroupInput(inputId = "language", label = "LANGUAGE", choices = c("Filipino", "English", "Others, please specify")),
+
+      # Number of Children
+      numericInput(inputId = "children", label = "NUMBER OF CHILDREN", min = 1, max = 100, value = 0, width = "80%"),
+
+      # Work Experience
+      sliderInput(inputId = "workexperience", label = "WORK EXPERIENCE IN YEARS", min = 0, max = 50, value = NULL, width = "80%"),
+
+      # Attached Photo
+      fileInput(inputId = "attachment", label = "UPLOAD PHOTO", accept = c(".png", ".jpg", ".svg"), multiple = FALSE, width = "80%")
     ),
     mainPanel(
       h4("SUMMARY"),
